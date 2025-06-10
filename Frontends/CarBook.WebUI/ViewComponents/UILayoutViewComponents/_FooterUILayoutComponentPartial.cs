64a0cb9c6124638923980604e7a4197b8b9f207e
Ömer Faruk Gündoğdu <omerrfarukgundogdu@gmail.com>
2025-06-10 19:22:14 +0300

    Footer işlemleri tamamlandı.

--- Frontends/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.FooterAddressDtos;
+using CarBook.Dto.SocialMediaDtos;
+using CarBook.WebUI.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _FooterUILayoutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var viewModel = new FooterViewModel
+            {
+                FooterAddresses = new List<ResultFooterAddressDto>(),
+                SocialMedias = new List<ResultSocialMediaDto>()
+            };
+
+
+            var footerTask = client.GetAsync("https://localhost:7031/api/FooterAddresses");
+            var socialMediaTask = client.GetAsync("https://localhost:7031/api/SocialMedias");
+
+            await Task.WhenAll(footerTask, socialMediaTask);
+
+
+            var footerAddressResponse = await footerTask;
+            if (footerAddressResponse.IsSuccessStatusCode)
+            {
+                var jsonData = await footerAddressResponse.Content.ReadAsStringAsync();
+                viewModel.FooterAddresses = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                
+            }
+
+            var socialMediaResponse = await socialMediaTask;
+            if (socialMediaResponse.IsSuccessStatusCode)
+            {
+                var jsonData = await socialMediaResponse.Content.ReadAsStringAsync();
+                viewModel.SocialMedias = JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData);
+            }
+
+            return View(viewModel);
         }
     }
 }
---
+++ Frontends/CarBook.WebUI/ViewModel/FooterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CarBook.Dto.FooterAddressDtos;
+using CarBook.Dto.SocialMediaDtos;
+
+namespace CarBook.WebUI.ViewModel
+{
+    public class FooterViewModel
+    {
+        public List<ResultFooterAddressDto> FooterAddresses { get; set; }
+        public List<ResultSocialMediaDto> SocialMedias { get; set; }
+    }
+}
