faba640ce82b3e33cb1ea9261a4f3d7e2d93a765
Ömer Faruk Gündoğdu <omerrfarukgundogdu@gmail.com>
2025-06-14 19:43:45 +0300

    Admin Dashboard sayfasının grafik içeriğini getirme işlemi tamamlandı.

---
+++ Frontends/CarBook.WebUI/Areas/Admin/ViewComponents/DashboardComponents/_AdminDashboardChart3ComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Areas.Admin.ViewComponents.DashboardComponents
+{
+    public class _AdminDashboardChart3ComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
